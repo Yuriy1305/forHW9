4d77b39579fa4b7430582a74dedcead5dedc2395
YuriyL <yuriy1305@gmail.com>
2022-04-17 22:24:02 +0200

    Commit9-2

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Course {
+import java.util.Objects;
+
+public class Course {
+    public String name;
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+// Переопределение для поиска одинаковых курсов у Студентов:
+    @Override
+    public boolean equals(Object o) {
+        Course course = (Course) o;
+        return Objects.equals(name, course.name);
+    }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Student {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Student {
+    private String name;
+    private List<Course> courses;
+
+    public Student(String name, List<Course> courses) {
+        this.name = name;
+        this.courses = new ArrayList<>(courses);
+    }
+    public String getName() {
+        return name;
+    }
+    public List<Course> getAllCourses() {
+        return Collections.unmodifiableList(courses);
+    }
+    @Override
+    public String toString() {
+        return name;
+    }
 }
